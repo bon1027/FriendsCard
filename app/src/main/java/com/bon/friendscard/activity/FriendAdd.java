c88d1085a0a36c34629ab4580eac896de81cac7a
BK93 <zxc3485@naver.com>
2016-06-27 12:51:39 +0900

    create friendAddActivity and layout

---
+++ app/src/main/java/com/bon/friendscard/activity/FriendAdd.java
@@ -0,0 +1,19 @@
+package com.bon.friendscard.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bon.friendscard.R;
+
+/**
+ * 友だち追加画面
+ */
+public class FriendAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_add);
+    }
+}
