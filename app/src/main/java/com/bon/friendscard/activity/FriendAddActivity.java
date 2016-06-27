6fba61f0d87a6f9e3bb82cb73536887ae0e748e0
BK93 <zxc3485@naver.com>
2016-06-27 12:55:59 +0900

    set floatingActionButton of friendAddActivity

--- app/src/main/java/com/bon/friendscard/activity/FriendAddActivity.java
+++ app/src/main/java/com/bon/friendscard/activity/FriendAddActivity.java
@@ -1,6 +1,9 @@
 package com.bon.friendscard.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,6 +13,11 @@ import com.bon.friendscard.R;
  * 友だち追加画面
  */
 public class FriendAddActivity extends AppCompatActivity {
+
+    @NonNull
+    public static Intent createIntent(@NonNull Context context) {
+        return new Intent(context, FriendAddActivity.class);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
--- app/src/main/java/com/bon/friendscard/activity/MainActivity.java
+++ app/src/main/java/com/bon/friendscard/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.bon.friendscard.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.bon.friendscard.R;
 
@@ -14,6 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton addFriendsButton = (FloatingActionButton) findViewById(R.id.add_friends_button);
+        if (addFriendsButton != null) {
+            addFriendsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // 友だち追加画面
+                    startActivity(FriendAddActivity.createIntent(MainActivity.this));
+                }
+            });
+        }
 
         setFriendList();
     }
