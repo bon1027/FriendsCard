414d122ec53af10f640add71082a83ebb415c9c4
BK93 <zxc3485@naver.com>
2016-06-27 12:24:58 +0900

    create setFriendList()

--- app/src/main/java/com/bon/friendscard/activity/MainActivity.java
+++ app/src/main/java/com/bon/friendscard/activity/MainActivity.java
@@ -1,18 +1,24 @@
 package com.bon.friendscard.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.bon.friendscard.R;
 
 /**
  * メイン画面
- * */
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setFriendList();
+    }
+
+    private void setFriendList() {
+
     }
 }
